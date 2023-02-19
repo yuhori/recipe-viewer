17a108721c883ca389154fdf5c28884189f00393
matteopratanamakmur <matteopratanamakmur.Main@gmail.com>
2023-02-19 21:18:47 +0900

    修正中

--- backend/configs/definition.go
+++ backend/configs/definition.go
@@ -1,10 +1,32 @@
 package configs
 
+import (
+	"github.com/spf13/viper"
+)
+
+var (
+	exists bool
+)
+
 func setDefaultConfig() {
 	// default 設定を書く場合には、ここに書く
 }
 
-func loadConfig error {
+func loadConfig() error {
 	setDefaultConfig()
-	
+
+	// 環境変数を優先
+	viper.AutomaticEnv()
+
+	exists = true
+	return nil
+}
+
+func GetString(k string) string {
+	if !exists {
+		if err := loadConfig(); err != nil {
+			panic(err)
+		}
+	}
+	return viper.GetString(k)
 }
--- backend/models/recipe.go
+++ backend/models/recipe.go
@@ -10,3 +10,12 @@ type Recipe struct {
 	Name string `json:"name"`
 	Link string `json:"link"`
 }
+
+func GetAll() ([]Recipe, error) {
+	var r []Recipe
+	result := Db.Find(&r)
+	if result.Error != nil {
+		return []Recipe{}, result.Error
+	}
+	return r, nil
+}
