becd9778341ea8fcbae04412b808c407c74a3647
matteopratanamakmur <matteopratanamakmur.Main@gmail.com>
2023-02-19 19:57:29 +0900

    修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={ homeUrl + "/list" } element={<List />} />
+        <Route path={ homeUrl + "/" } element={<List />} />
       </Routes>
     </div>
   );
