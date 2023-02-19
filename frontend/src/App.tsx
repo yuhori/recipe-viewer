805f18c23a8656dd9c74335923009ac98cd88684
matteopratanamakmur <matteopratanamakmur.Main@gmail.com>
2023-02-19 16:08:01 +0900

    修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import List from './pages/List';
 
+const homeUrl = process.env.PUBLIC_URL;
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/recipe-viewer/list" element={<List />} />
+        <Route path={ homeUrl + "/list" } element={<List />} />
       </Routes>
     </div>
   );
